5c222581155182b0973251fd76ca27ec83171032
arandombird671 <arandombird671@gmail.com>
2024-10-21 21:10:40 +0300

    side navbar extension

--- Frontend/src/components/Navbars/SideNavbar/SideNavbar.tsx
+++ Frontend/src/components/Navbars/SideNavbar/SideNavbar.tsx
@@ -16,64 +16,82 @@ import {
 import { DisplayXLUp } from "@/assets/XL breakpoint/DisplayXLup";
 import { DisplayBelowXL } from "@/assets/XL breakpoint/DisplayBelowXL";
 import { useState } from "react";
+import { SearchBar } from "@/components/SearchBar/SearchBar";
 
 export const SideNavbar = () => {
   const [showExtension, setShowExtension] = useState<boolean>(false);
+  const [searchResults, setSearchResults] = useState<any[]>([]);
   return (
     <div className={styles.container}>
-      <div className="mt-2 mb-3">
-        <DisplayBelowXL>
-          <InstaCloneNavbarButton />
-        </DisplayBelowXL>
-        {showExtension && (
-          <DisplayXLUp>
+      <div className={styles.navbar}>
+        <div className="mt-2 mb-3">
+          <DisplayBelowXL>
             <InstaCloneNavbarButton />
-          </DisplayXLUp>
-        )}
-        {!showExtension && (
-          <DisplayXLUp>
-            <InstaCloneTextLogoButton />
-          </DisplayXLUp>
-        )}
-      </div>
-      <div>
-        <HomeNavbarButton />
-        {!showExtension && <DisplayXLUp>Home</DisplayXLUp>}
-      </div>
-      <div>
-        <SearchNavbarButton onClick={() => setShowExtension((prev) => !prev)} />
-        {!showExtension && <DisplayXLUp>Search</DisplayXLUp>}
-      </div>
-      <div>
-        <ExploreNavbarButton />
+          </DisplayBelowXL>
+          {showExtension && (
+            <DisplayXLUp>
+              <InstaCloneNavbarButton />
+            </DisplayXLUp>
+          )}
+          {!showExtension && (
+            <DisplayXLUp>
+              <InstaCloneTextLogoButton />
+            </DisplayXLUp>
+          )}
+        </div>
+        <div>
+          <HomeNavbarButton />
+          {!showExtension && <DisplayXLUp>Home</DisplayXLUp>}
+        </div>
+        <div>
+          <SearchNavbarButton
+            onClick={() => setShowExtension((prev) => !prev)}
+          />
+          {!showExtension && <DisplayXLUp>Search</DisplayXLUp>}
+        </div>
+        <div>
+          <ExploreNavbarButton />
 
-        {!showExtension && <DisplayXLUp>Explore</DisplayXLUp>}
-      </div>
-      <div>
-        <ReelsNavbarButton />
-        {!showExtension && <DisplayXLUp>Reels</DisplayXLUp>}
-      </div>
-      <div>
-        <MessengerNavbarButton />
-        {!showExtension && <DisplayXLUp>Messages</DisplayXLUp>}
-      </div>
-      <div>
-        <HeartNavbarButton />
-        {!showExtension && <DisplayXLUp>Notifications</DisplayXLUp>}
-      </div>
-      <div>
-        <PlusInsideSquareNavbarButton />
-        {!showExtension && <DisplayXLUp>Create</DisplayXLUp>}
-      </div>
-      <div>
-        <PFPNavbarButton />
-        {!showExtension && <DisplayXLUp>Profile</DisplayXLUp>}
-      </div>
+          {!showExtension && <DisplayXLUp>Explore</DisplayXLUp>}
+        </div>
+        <div>
+          <ReelsNavbarButton />
+          {!showExtension && <DisplayXLUp>Reels</DisplayXLUp>}
+        </div>
+        <div>
+          <MessengerNavbarButton />
+          {!showExtension && <DisplayXLUp>Messages</DisplayXLUp>}
+        </div>
+        <div>
+          <HeartNavbarButton />
+          {!showExtension && <DisplayXLUp>Notifications</DisplayXLUp>}
+        </div>
+        <div>
+          <PlusInsideSquareNavbarButton />
+          {!showExtension && <DisplayXLUp>Create</DisplayXLUp>}
+        </div>
+        <div>
+          <PFPNavbarButton />
+          {!showExtension && <DisplayXLUp>Profile</DisplayXLUp>}
+        </div>
 
-      <div className="mt-4">
-        <ListNavbarButton />
-        {!showExtension && <DisplayXLUp>More</DisplayXLUp>}
+        <div className="mt-4">
+          <ListNavbarButton />
+          {!showExtension && <DisplayXLUp>More</DisplayXLUp>}
+        </div>
       </div>
+      {showExtension && (
+        <div className={styles.navbarExtension}>
+          <div className="my-3">
+            <h5 className={`${styles.text} mt-2 mb-4`}>Search</h5>
+            <SearchBar setSearchResults={setSearchResults} />
+          </div>
+          <div className="w-100 border-top">
+            <div className={`${styles.text} mt-3`}>Recent</div>
+            <div></div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
