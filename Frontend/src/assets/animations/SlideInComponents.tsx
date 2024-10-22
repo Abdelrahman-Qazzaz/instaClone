77f69ab716713bd7bd4f6340e009bc523540fe9c
arandombird671 <arandombird671@gmail.com>
2024-10-22 23:17:10 +0300

    refactor slide in component

--- Frontend/src/assets/animations/SlideInComponents.tsx
+++ Frontend/src/assets/animations/SlideInComponents.tsx
@@ -1,15 +1,32 @@
-import { motion } from "framer-motion";
-import { ReactNode } from "react";
-export const SlideInLeftToRight = ({ children }: { children: ReactNode }) => {
+import { AnimatePresence, motion } from "framer-motion";
+import { ReactNode, useEffect, useState } from "react";
+export const SlideInLeftToRight = ({
+  children,
+  show,
+}: {
+  children: ReactNode;
+  show: boolean;
+}) => {
+  const [showChildren, setShowChildren] = useState(false);
+
   return (
-    <motion.div
-      initial={{ width: 0 }}
-      animate={{ width: "fit-content" }}
-      exit={{ width: 0 }}
-      transition={{ duration: 1 }}
-    >
-      {children}
-    </motion.div>
+    <AnimatePresence>
+      {show && (
+        <motion.div
+          initial={{ width: 0 }}
+          animate={{ width: "fit-content" }}
+          exit={{ width: 0 }}
+          transition={{ duration: 0.4 }}
+          onAnimationComplete={() => {
+            setShowChildren(true);
+          }}
+        >
+          <div style={{ visibility: showChildren ? "visible" : "hidden" }}>
+            {children}
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
