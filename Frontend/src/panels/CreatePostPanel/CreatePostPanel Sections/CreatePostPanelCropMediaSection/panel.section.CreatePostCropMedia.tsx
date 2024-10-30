1436576d7aaaf4e8c9ff7f3602394942920e4a83
arandombird671 <arandombird671@gmail.com>
2024-10-30 22:02:13 +0300

    basic cropper

--- Frontend/src/panels/CreatePostPanel/CreatePostPanel Sections/CreatePostPanelCropMediaSection/panel.section.CreatePostCropMedia.tsx
+++ Frontend/src/panels/CreatePostPanel/CreatePostPanel Sections/CreatePostPanelCropMediaSection/panel.section.CreatePostCropMedia.tsx
@@ -1,7 +1,6 @@
 import { previewFile } from "@/panels/CreatePostPanel/panel.CreatePost";
 import { useState } from "react";
 import ReactCrop, { type Crop } from "react-image-crop";
-import styles from "./panel.section.CreatePostCropMedia.module.css";
 
 export const CreatePostPanelCropMediaSection = ({
   previewFiles,
@@ -10,17 +9,28 @@ export const CreatePostPanelCropMediaSection = ({
 }) => {
   const [crop, setCrop] = useState<Crop>();
   return (
-    <div className={styles.container}>
+    <>
       {previewFiles.map((previewFile) => (
         <ReactCrop
-          className={styles.ReactCrop}
           crop={crop}
-          onChange={(c) => setCrop(c)}
+          onChange={(c) => {
+            console.log(c);
+            /*
+            {
+  "x": 33.2421875,
+  "y": 23.921875,
+  "width": 365.92578125,
+  "height": 224.40625,
+  "unit": "px"
+}
+            */
+            setCrop(c);
+          }}
         >
           {previewFile.type === "image" && <img src={previewFile.src} />}
           {previewFile.type === "video" && <video src={previewFile.src} />}
         </ReactCrop>
       ))}
-    </div>
+    </>
   );
 };
